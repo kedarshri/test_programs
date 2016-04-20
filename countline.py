bd40d87b18bb949dce3200032c434fb113d699ba
kedarshri <kedarshri@gmail.com>
2016-04-19 23:07:01 -0700

    Added files via upload
    updated for printing first 100 characters

--- countline.py
+++ countline.py
@@ -5,6 +5,10 @@ def main():
    for line in fhandle:
        count= count+1
    print 'Number of Lines:',count
+   fhandle = open (filename)
+   input = fhandle.read()
+   print len(input)
+   print input[:100]
 
 if __name__ == "__main__":
      main()
